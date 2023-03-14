4ea5e0f49eb40e985435b059c3dfcb40a08f9ddd
ArionLaw <87771407+ArionLaw@users.noreply.github.com>
2023-03-13 23:50:07 -0400

    initial upload of scripts

---
+++ Workspace Scripts/Workspace Analysis FK from AMBF.py
@@ -0,0 +1,19 @@
+import vedo
+import numpy as np
+from From_Github.ambf.ambf_controller.dvrk.scripts.psmFK import* 
+
+# joint limits
+q1_lim = [0 , 90]
+q2_lim = [0 , 90]
+q3_lim = [0 , 90]
+q4_lim = [0 , 90]
+q5_lim = [0 , 90]
+q6_lim = [0 , 90]
+step = 5 #rad
+
+joint_pos = [0 , 0 , 0 , 0 , 0 , 0]
+base_frame_pos = np.mat([[0],[0],[0],[0]])
+EE_pos = np.matmul(compute_FK(joint_pos),base_frame_pos)
+print(EE_pos)
+print(compute_FK(joint_pos))
+
